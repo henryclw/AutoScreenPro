ffb59914f3fefe111e61e5c2925acf9ec36e33c3
Henry <97804910+henryclw@users.noreply.github.com>
2023-03-21 00:32:02 -0700

    Use logging in screenshot_processor.py

--- screenshot_processor.py
+++ screenshot_processor.py
@@ -1,3 +1,4 @@
+import logging
 import time
 from typing import List
 
@@ -16,14 +17,14 @@ class ScreenshotProcessor:
     def __init__(self, pil_image: Image):
         self.pil_image = pil_image
         self.image_array = np.asarray(self.pil_image)
-        print(self.image_array.shape)
+        logging.info("ScreenshotProcessor self.image_array.shape is {}".format(self.image_array.shape))
 
     # def __del__(self):
     #     self.pil_image.close()
 
     def _get_gray_array(self):
         gray = cv.cvtColor(self.image_array, cv.COLOR_BGR2GRAY)
-        print(gray.shape)
+        logging.info("ScreenshotProcessor Gray array shape is {}".format(gray.shape))
         return gray
 
     def _get_cut_line_position_by_gray(self, gray: np.ndarray):
@@ -32,7 +33,8 @@ class ScreenshotProcessor:
             # for i in range(1850, 1860):
             this_line = gray[i, :]
             if 226 < np.median(this_line) < 229:
-                print(i, this_line.max(), this_line.min(), np.median(this_line), this_line.mean())
+                logging.info("In _get_cut_line_position_by_gray i: {}, max: {}, min: {}, median: {}, mean: {}"
+                             .format(i, this_line.max(), this_line.min(), np.median(this_line), this_line.mean()))
                 cut_x_list.append(i)
         return cut_x_list
 
@@ -46,12 +48,13 @@ class ScreenshotProcessor:
         gray = self._get_gray_array()
         cut_x_list = self._get_cut_line_position_by_gray(gray)
         self._save_wechat_moment(cut_x_list, "E:/data/AutoScreenPro/temp/")
-        print(cut_x_list)
-        if len(cut_x_list) >= 1:
+        logging.info("In cut_this_wechat_moment cut_x_list: {}".format(cut_x_list))
+        if len(cut_x_list) > 1:
             swipe_down_distance_for_next_moment = cut_x_list[1] - 240
         else:
             swipe_down_distance_for_next_moment = 1800
-        print(swipe_down_distance_for_next_moment)
+        logging.info("In cut_this_wechat_moment swipe_down_distance_for_next_moment: {}"
+                     .format(swipe_down_distance_for_next_moment))
         return swipe_down_distance_for_next_moment
 
 
