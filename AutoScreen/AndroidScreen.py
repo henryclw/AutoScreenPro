2df3f0f246939bf6f6a8dfa6998b10d10fb08c6d
Henry <97804910+henryclw@users.noreply.github.com>
2024-01-30 02:01:50 -0800

    Good run of 10 rounds

--- AutoScreen/AndroidScreen.py
+++ AutoScreen/AndroidScreen.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import random
 import subprocess
 import time
 from typing import List
@@ -52,6 +53,12 @@ class AndroidController:
         self.xml_dir = config.android_xml_dir
         self.width, self.height = self.get_device_size()
         self.backslash = "\\"
+        self.screen_width, self.screen_height = self.get_device_size()
+
+    def get_device_size(self):
+        adb_command = f"adb -s {self.device} shell wm size"
+        result = ADBPropertiesHelper.run_adb_command(adb_command)
+        return map(int, result.split(": ")[1].split("x"))
 
     def set_show_touches(self, flag: bool):
         ADBPropertiesHelper.run_adb_command(f"adb shell -s {self.device} settings put system show_touches {'1' if flag else '0'}")
@@ -64,22 +71,20 @@ class AndroidController:
         ADBPropertiesHelper.run_adb_command(
             f"adb -s {self.device} shell setprop debug.layout {'true' if flag else 'false'} && adb shell -s {self.device} service call activity 1599295570")
 
+    def press_back_key(self):
+        ADBPropertiesHelper.run_adb_command(f"adb -s {self.device} shell input keyevent KEYCODE_BACK")  # KEYCODE_BACK=4
+
     def press_home_key(self):
-        ADBPropertiesHelper.run_adb_command(f"adb -s {self.device} shell input keyevent 3")
+        ADBPropertiesHelper.run_adb_command(f"adb -s {self.device} shell input keyevent KEYCODE_HOME")  # KEYCODE_HOME=3
 
     def press_menu_key_twice(self):
-        ADBPropertiesHelper.run_adb_command(f'adb -s {self.device} shell "input keyevent 82 && input keyevent 82"')
+        ADBPropertiesHelper.run_adb_command(f'adb -s {self.device} shell "input keyevent KEYCODE_MENU && input keyevent KEYCODE_MENU"')  # KEYCODE_MENU=82
 
     def reopen_app_by_going_to_homescreen(self):
         logging.info(f"reopen_app_by_going_to_homescreen with device {self.device}")
         self.press_home_key()
         time.sleep(3)
         self.press_menu_key_twice()
-
-    def get_device_size(self):
-        adb_command = f"adb -s {self.device} shell wm size"
-        result = ADBPropertiesHelper.run_adb_command(adb_command)
-        return map(int, result.split(": ")[1].split("x"))
 
     def get_screenshot(self, filename, save_dir):
         cap_command = f"adb -s {self.device} shell screencap -p " \
@@ -107,55 +112,64 @@ class AndroidController:
         ADBPropertiesHelper.run_adb_command(delete_file_command)
         return os.path.join(save_dir, filename + ".xml")
 
-    def back(self):
-        adb_command = f"adb -s {self.device} shell input keyevent KEYCODE_BACK"
-        ret = ADBPropertiesHelper.run_adb_command(adb_command)
-        return ret
+    def swipe(self, start_x: int, start_y: int, end_x: int, end_y: int, duration: int = None):
+        if duration is None:
+            duration = abs(start_y - start_x) * random.randrange(4, 10)  # too fast would leave some momentum
+        logging.info(f"swipe called, start_x={start_x}, start_y={start_y}, end_x={end_x}, end_y={end_y}, duration={duration}")
+        adb_command = f"adb -s {self.device} shell input swipe {start_x} {start_y} {end_x} {end_y} {duration}"
+        ADBPropertiesHelper.run_adb_command(adb_command)
 
-    def tap(self, x, y):
-        adb_command = f"adb -s {self.device} shell input tap {x} {y}"
-        ret = ADBPropertiesHelper.run_adb_command(adb_command)
-        return ret
+    def swipe_up(self, diff_height: int):
+        logging.info(f"swipe_up called, diff_height={diff_height}")
+        diff_width = random.randrange(-320, 320)
+        start_x = random.randrange(abs(diff_width), self.screen_width - abs(diff_width))
+        start_y = random.randrange(250 + diff_height, self.screen_height)  # don't want to end up too high
+        self.swipe(start_x, start_y, start_x + diff_width, start_y - diff_height)
 
-    def text(self, input_str):
-        input_str = input_str.replace(" ", "%s")
-        input_str = input_str.replace("'", "")
-        adb_command = f"adb -s {self.device} shell input text {input_str}"
-        ret = ADBPropertiesHelper.run_adb_command(adb_command)
-        return ret
-
-    def long_press(self, x, y, duration=1000):
-        adb_command = f"adb -s {self.device} shell input swipe {x} {y} {x} {y} {duration}"
-        ret = ADBPropertiesHelper.run_adb_command(adb_command)
-        return ret
-
-    def swipe(self, x, y, direction, dist="medium", quick=False):
-        unit_dist = int(self.width / 10)
-        if dist == "long":
-            unit_dist *= 3
-        elif dist == "medium":
-            unit_dist *= 2
-        if direction == "up":
-            offset = 0, -2 * unit_dist
-        elif direction == "down":
-            offset = 0, 2 * unit_dist
-        elif direction == "left":
-            offset = -1 * unit_dist, 0
-        elif direction == "right":
-            offset = unit_dist, 0
-        else:
-            raise RuntimeError(f"Unknown direction: {direction}")
-        duration = 100 if quick else 400
-        adb_command = f"adb -s {self.device} shell input swipe {x} {y} {x+offset[0]} {y+offset[1]} {duration}"
-        ret = ADBPropertiesHelper.run_adb_command(adb_command)
-        return ret
-
-    def swipe_precise(self, start, end, duration=400):
-        start_x, start_y = start
-        end_x, end_y = end
-        adb_command = f"adb -s {self.device} shell input swipe {start_x} {start_x} {end_x} {end_y} {duration}"
-        ret = ADBPropertiesHelper.run_adb_command(adb_command)
-        return ret
+    # def tap(self, x, y):
+    #     adb_command = f"adb -s {self.device} shell input tap {x} {y}"
+    #     ret = ADBPropertiesHelper.run_adb_command(adb_command)
+    #     return ret
+    #
+    # def text(self, input_str):
+    #     input_str = input_str.replace(" ", "%s")
+    #     input_str = input_str.replace("'", "")
+    #     adb_command = f"adb -s {self.device} shell input text {input_str}"
+    #     ret = ADBPropertiesHelper.run_adb_command(adb_command)
+    #     return ret
+    #
+    # def long_press(self, x, y, duration=1000):
+    #     adb_command = f"adb -s {self.device} shell input swipe {x} {y} {x} {y} {duration}"
+    #     ret = ADBPropertiesHelper.run_adb_command(adb_command)
+    #     return ret
+    #
+    # def swipe(self, x, y, direction, dist="medium", quick=False):
+    #     unit_dist = int(self.width / 10)
+    #     if dist == "long":
+    #         unit_dist *= 3
+    #     elif dist == "medium":
+    #         unit_dist *= 2
+    #     if direction == "up":
+    #         offset = 0, -2 * unit_dist
+    #     elif direction == "down":
+    #         offset = 0, 2 * unit_dist
+    #     elif direction == "left":
+    #         offset = -1 * unit_dist, 0
+    #     elif direction == "right":
+    #         offset = unit_dist, 0
+    #     else:
+    #         raise RuntimeError(f"Unknown direction: {direction}")
+    #     duration = 100 if quick else 400
+    #     adb_command = f"adb -s {self.device} shell input swipe {x} {y} {x+offset[0]} {y+offset[1]} {duration}"
+    #     ret = ADBPropertiesHelper.run_adb_command(adb_command)
+    #     return ret
+    #
+    # def swipe_precise(self, start, end, duration=400):
+    #     start_x, start_y = start
+    #     end_x, end_y = end
+    #     adb_command = f"adb -s {self.device} shell input swipe {start_x} {start_x} {end_x} {end_y} {duration}"
+    #     ret = ADBPropertiesHelper.run_adb_command(adb_command)
+    #     return ret
 
 
 class UINode:
