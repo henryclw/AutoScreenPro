39a2a6fec7e4defce9428a6e9ccc1b33ea2652e1
Henry <97804910+henryclw@users.noreply.github.com>
2024-01-30 03:05:49 -0800

    Create saving picture, use menu and tap to get the previous app

--- AutoScreen/AndroidScreen.py
+++ AutoScreen/AndroidScreen.py
@@ -77,14 +77,16 @@ class AndroidController:
     def press_home_key(self):
         ADBPropertiesHelper.run_adb_command(f"adb -s {self.device} shell input keyevent KEYCODE_HOME")  # KEYCODE_HOME=3
 
-    def press_menu_key_twice(self):
-        ADBPropertiesHelper.run_adb_command(f'adb -s {self.device} shell "input keyevent KEYCODE_MENU && input keyevent KEYCODE_MENU"')  # KEYCODE_MENU=82
+    def get_back_to_last_app(self):
+        ADBPropertiesHelper.run_adb_command(f'adb -s {self.device} shell "input keyevent KEYCODE_MENU"')  # KEYCODE_MENU=82
+        time.sleep(1)
+        self.tap(x=295, y=1111)
 
     def reopen_app_by_going_to_homescreen(self):
         logging.info(f"reopen_app_by_going_to_homescreen with device {self.device}")
         self.press_home_key()
-        time.sleep(3)
-        self.press_menu_key_twice()
+        time.sleep(2)
+        self.get_back_to_last_app()
 
     def get_screenshot(self, filename, save_dir):
         cap_command = f"adb -s {self.device} shell screencap -p " \
@@ -112,9 +114,34 @@ class AndroidController:
         ADBPropertiesHelper.run_adb_command(delete_file_command)
         return os.path.join(save_dir, filename + ".xml")
 
+    def copy_and_remove_the_latest_file(self, folder_in_android: str, save_dir_in_host: str):
+        ls_command = f"adb -s {self.device} shell ls {folder_in_android} -tp | grep -v /$ | head -1"
+        filename = ADBPropertiesHelper.run_adb_command(ls_command)
+        pull_command = f"adb -s {self.device} pull {folder_in_android}/{filename} {save_dir_in_host.replace(self.backslash, '/')}/{filename}"
+        delete_file_command = f"adb -s {self.device} shell rm {folder_in_android}/{filename}"
+        ADBPropertiesHelper.run_adb_command(pull_command)
+        ADBPropertiesHelper.run_adb_command(delete_file_command)
+        return os.path.join(save_dir_in_host, filename)
+
+    def tap(self, x: int, y: int):
+        logging.info(f"tap called, x={x}, y={y}")
+        adb_command = f"adb -s {self.device} shell input tap {x} {y}"
+        ADBPropertiesHelper.run_adb_command(adb_command)
+
+    def tap_in_box(self, left: int, high: int, right: int, low: int):
+        x = random.randrange(left + 2, right - 2)
+        y = random.randrange(high + 2, low - 2)
+        self.tap(x, y)
+
+    def long_press(self, x: int, y: int, duration: int = 1200, end_x: int = None, end_y: int = None):
+        duration = random.randrange(1000, 2000)
+        end_x = x + random.randrange(-20, 20)
+        end_y = y + random.randrange(-30, 30)
+        self.swipe(start_x=x, start_y=y, end_x=end_x, end_y=end_y, duration=duration)
+
     def swipe(self, start_x: int, start_y: int, end_x: int, end_y: int, duration: int = None):
         if duration is None:
-            duration = abs(start_y - start_x) * random.randrange(4, 10)  # too fast would leave some momentum
+            duration = abs(start_y - start_x) * random.randrange(4, 7)  # too fast would leave some momentum
         logging.info(f"swipe called, start_x={start_x}, start_y={start_y}, end_x={end_x}, end_y={end_y}, duration={duration}")
         adb_command = f"adb -s {self.device} shell input swipe {start_x} {start_y} {end_x} {end_y} {duration}"
         ADBPropertiesHelper.run_adb_command(adb_command)
@@ -122,24 +149,14 @@ class AndroidController:
     def swipe_up(self, diff_height: int):
         logging.info(f"swipe_up called, diff_height={diff_height}")
         diff_width = random.randrange(-320, 320)
-        start_x = random.randrange(abs(diff_width), self.screen_width - abs(diff_width))
+        start_x = random.randrange(abs(diff_width) + 120, self.screen_width - abs(diff_width) - 120)
         start_y = random.randrange(250 + diff_height, self.screen_height)  # don't want to end up too high
         self.swipe(start_x, start_y, start_x + diff_width, start_y - diff_height)
-
-    # def tap(self, x, y):
-    #     adb_command = f"adb -s {self.device} shell input tap {x} {y}"
-    #     ret = ADBPropertiesHelper.run_adb_command(adb_command)
-    #     return ret
     #
     # def text(self, input_str):
     #     input_str = input_str.replace(" ", "%s")
     #     input_str = input_str.replace("'", "")
     #     adb_command = f"adb -s {self.device} shell input text {input_str}"
-    #     ret = ADBPropertiesHelper.run_adb_command(adb_command)
-    #     return ret
-    #
-    # def long_press(self, x, y, duration=1000):
-    #     adb_command = f"adb -s {self.device} shell input swipe {x} {y} {x} {y} {duration}"
     #     ret = ADBPropertiesHelper.run_adb_command(adb_command)
     #     return ret
     #
